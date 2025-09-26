d6340c217ef15039c9065da6ecfe9f7c3245a2e7
Holger Steinhaus <holger.steinhaus@emobotics.dev>
2025-09-26 16:34:59 +0200

    main: show heap stats

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@
 extern crate alloc;
 
 use defmt;
+use defmt::info;
 use esp_backtrace as _;
 use esp_println as _;
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-
+use esp_alloc::HeapStats;
+use esp_println::println;
 use crate::ble_scan::ble_scan_task;
 use crate::board::startup;
 use crate::ui::ui_task;
@@ -28,18 +30,22 @@ mod util;
 async fn main(spawner: Spawner) {
     let mut res = startup::Resources::initialize();
     // Draw a smiley face with white eyes and a red mouth
-    res.display.draw_smiley().unwrap();
+    //res.display.draw_smiley().unwrap();
 
     // Spawn ble_scan::run in a separate Embassy task
     spawner
         .spawn(ble_scan_task(res.wifi_ble.ble_connector))
         .expect("Failed to spawn ble_scan_task");
 
-    spawner.spawn(ui_task(res.display));
+    spawner
+        .spawn(ui_task(res.display))
+        .expect("Failed to spawn ui_task");
 
     defmt::info!("Embassy initialized!");
     loop {
-        defmt::info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+        let stats: HeapStats = esp_alloc::HEAP.stats();
+        info!("mainloop");
+        println!("{}", stats);
+        Timer::after(Duration::from_secs(5)).await;
     }
 }
