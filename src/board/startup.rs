d898968551ea67f023743349b046fc0e227d710e
Holger Steinhaus <holger.steinhaus@emobotics.dev>
2025-10-15 08:57:23 +0200

    startup: prepare SPI sharing

--- src/board/startup.rs
+++ src/board/startup.rs
@@ -1,20 +1,9 @@
+use core::cell::RefCell;
 use async_button::{Button, ButtonConfig};
-use embedded_hal_bus::spi::ExclusiveDevice;
-use esp_hal::{
-    clock::CpuClock,
-    delay::Delay,
-    dma::{DmaRxBuf, DmaTxBuf},
-    dma_buffers,
-    gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull},
-    i2c::master::{BusTimeout, Config as I2cConfig, I2c},
-    rng::Rng,
-    spi::master::{Config as SpiConfig, Spi},
-    spi::Mode,
-    system::CpuControl,
-    time::Rate,
-    timer::{timg::TimerGroup, AnyTimer},
-};
-
+use embedded_hal_bus::spi::RefCellDevice;
+use esp_hal::{clock::CpuClock, delay::Delay, dma::{DmaRxBuf, DmaTxBuf}, dma_buffers, gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull}, i2c::master::{BusTimeout, Config as I2cConfig, I2c}, rng::Rng, spi::master::{Config as SpiConfig, Spi}, spi::Mode, system::CpuControl, time::Rate, timer::{timg::TimerGroup, AnyTimer}, Async};
+use esp_hal::spi::master::SpiDmaBus;
+use static_cell::make_static;
 use crate::board::driver::{
     analog::{AdcDriver, AdcDriverType},
     display::DisplayDriver,
@@ -74,10 +63,6 @@ impl Resources {
         let button_right = Button::new(button_right, ButtonConfig::default());
 
         ////////////////////////// Display init ////////////////////////////
-        let sclk = peripherals.GPIO18;
-        let mosi = peripherals.GPIO23;
-        let cs = peripherals.GPIO14;
-        let bl = peripherals.GPIO32;
 
         #[allow(clippy::manual_div_ceil)]
         let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(4092);
@@ -85,25 +70,27 @@ impl Resources {
         let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
         let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
 
-        let spi = Spi::new(
+        let spi_bus = Spi::new(
             peripherals.SPI2,
             SpiConfig::default()
                 .with_frequency(Rate::from_mhz(40))
                 .with_mode(Mode::_0),
         )
-        .unwrap()
-        .with_sck(sclk)
-        .with_mosi(mosi) // order matters
-        .with_dma(display_dma_channel)
-        .with_buffers(dma_rx_buf, dma_tx_buf)
-        .into_async();
+            .unwrap()
+            .with_sck(peripherals.GPIO18)
+            .with_mosi(peripherals.GPIO23) // order matters
+            .with_miso(peripherals.GPIO19)
+            .with_dma(display_dma_channel)
+            .with_buffers(dma_rx_buf, dma_tx_buf)
+            .into_async();
 
-        let bl = Output::new(bl, Level::High, OutputConfig::default());
-        let cs = Output::new(cs, Level::High, OutputConfig::default());
+        let cs_display = Output::new(peripherals.GPIO14, Level::High, OutputConfig::default());
+        let shared_spi_bus = make_static!(RefCell::new(spi_bus));
+        let spi_device = RefCellDevice::new(shared_spi_bus, cs_display, Delay::new()).unwrap();
+
+        let bl = Output::new(peripherals.GPIO32, Level::High, OutputConfig::default());
         let dc = Output::new(peripherals.GPIO27, Level::Low, OutputConfig::default());
         let rst = Output::new(peripherals.GPIO33, Level::Low, OutputConfig::default());
-        let delay = Delay::new();
-        let spi_device = ExclusiveDevice::new(spi, cs, delay).unwrap();
         let d = DisplayDriver::new(spi_device, bl, rst, dc);
 
         ////////////////////////// PPS Module init ////////////////////////////
