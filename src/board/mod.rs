a67ab2400091276a743663c1324e016099599bb3
Holger Steinhaus <holger.steinhaus@emobotics.dev>
2025-10-15 17:57:26 +0200

    reset instead of halt on panic - this ensures that the PPS will switch off the field current

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,3 +1,5 @@
+use esp_hal::rom::software_reset;
+
 pub mod driver;
 pub mod startup;
 pub mod io;
@@ -7,4 +9,10 @@ esp_bootloader_esp_idf::esp_app_desc!();
 #[no_mangle]
 pub extern "Rust" fn _esp_println_timestamp() -> u64 {
     esp_hal::time::Instant::now().duration_since_epoch().as_millis()
+}
+
+#[no_mangle]
+fn custom_halt() -> ! {
+    error!("custom_halt - resetting...");
+    software_reset();
 }
